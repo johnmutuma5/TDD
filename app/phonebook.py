644ab670bb61f0140b4e3c961a3d33c91d140ed3
John Mutuma <johnmutuma5@gmail.com>
2018-03-01 10:11:18 +0300

    Add testing for the Phonebook's fuctionality 	* test add contact 	* test update contact 	* test virw contact 	* test delete contact

--- app/phonebook.py
+++ app/phonebook.py
@@ -1,13 +1,17 @@
 def main ():
-    contact = Contact ('Lazuli', 'Muthoni', '0728655088')
-    phonebook = Phonebook()
+    ''' Main program code here
+    '''
+    ...
 
-    last_name = phonebook.add_contact (contact)
-    print (last_name)
 
 
 
 class Contact ():
+
+    '''
+        Phonebook: contains a contacts basic data; names are case-sensitive
+        params: first_name, last_name, mobile
+    '''
     def __init__ (self, first_name, last_name, mobile):
         self.first_name = first_name
         self.last_name = last_name
@@ -23,14 +27,53 @@ class Contact ():
 
 
 class Phonebook():
+    '''
+        Phonebook: contains a list of contacts
+            methods:    add_contact
+                            params: Contact object
+                        update_contact
+                            paramas: first_name, last_name, new_first, new_last, new_mobile
+                        delete_contact
+                            params: first_name, last_name
+                        view_contact
+                            params: first_name, last_name
+            params:     none
+    '''
+
     def __init__(self):
         self.contacts = []
 
     def add_contact (self, contact):
+
         self.contacts.append(contact)
         pushed_contact = self.contacts[-1]
         return pushed_contact.first_name
 
+    def update_contact (self, first_name, last_name,
+                        new_first = None, new_last = None, new_mobile = None):
+
+        for contact in self.contacts:
+            if contact.first_name == first_name and contact.last_name == last_name:
+                if new_first: contact.first_name = new_first
+                if new_last: contact.last_name =  new_last
+                if new_mobile: contact.mobile =  new_mobile
+
+                return contact
+                break
+
+    def delete_contact (self, first_name, last_name):
+
+        for contact in self.contacts:
+            if contact.first_name == first_name and contact.last_name == last_name:
+                del contact
+                break
+
+    def view_contact (self, first_name, last_name):
+        
+        for contact in self.contacts:
+            if contact.first_name == first_name and contact.last_name == last_name:
+                return contact.mobile
+                break
 
 
 #call main
--- phonebook_test.py
+++ phonebook_test.py
@@ -4,11 +4,77 @@ from app.phonebook import Phonebook, Contact
 
 class PhonebookTestCase (unittest.TestCase):
 
+    def __init__ (self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.phonebook = Phonebook()
+
+
+
     def test_add_contact (self):
+
         contact = Contact ('Lazuli', 'Muthoni', '0728655088')
-        phonebook = Phonebook()
-        first_name = phonebook.add_contact (contact)
+        first_name = self.phonebook.add_contact (contact)
         self.assertEqual (first_name, 'Lazuli')
+
+    def test_update_contact (self):
+
+        contact = Contact ('Lazuli', 'Muthoni', '0728655088')
+        self.phonebook.add_contact (contact)
+
+        # test update first_name alone
+        updated_contact = self.phonebook.update_contact("Lazuli", "Muthoni", new_first = "Lazuli Olive")
+        self.assertEqual(updated_contact.first_name, 'Lazuli Olive')
+
+        # test update last_name alone
+        updated_contact = self.phonebook.update_contact("Lazuli Olive", "Muthoni", new_last = "Murimi")
+        self.assertEqual (updated_contact.last_name, 'Murimi')
+
+        # test update mobile number alone
+        updated_contact = self.phonebook.update_contact("Lazuli Olive", "Murimi", new_mobile = "0725280260")
+        self.assertEqual (updated_contact.mobile, '0725280260')
+
+        # test update All
+        updated_contact = self.phonebook.update_contact("Lazuli Olive", "Murimi", new_mobile = "0728655088",
+                                                            new_last = 'Muthoni',
+                                                            new_first = 'Lazuli')
+        self.assertEqual (updated_contact.mobile, '0728655088')
+        self.assertEqual (updated_contact.first_name, 'Lazuli')
+        self.assertEqual (updated_contact.last_name, 'Muthoni')
+
+
+    def test_delete_contact (self):
+
+        contact = Contact ('Lazuli', 'Muthoni', '0728655088')
+        self.phonebook.add_contact (contact)
+
+        # add a second contact
+        contact = Contact ('Sheelah', 'B', '0725280260')
+        self.phonebook.add_contact (contact)
+
+        # delete second contact
+        self.phonebook.delete_contact ('Sheelah', 'B')
+
+        # check if the contact still exists
+        contact = None
+        for contact in self.phonebook.contacts:
+            if contact.first_name == 'Sheelah' and contact.last_name == 'B':
+                contact = contact
+                break
+        #if contact is not None, then it was not deleted successfully
+        self.assertNotEqual(contact, None)
+
+
+    def test_view_contact(self):
+
+            contact = Contact ('Lazuli', 'Muthoni', '0728655088')
+            self.phonebook.add_contact (contact)
+
+            mobile = self.phonebook.view_contact('Lazuli', 'Muthoni')            
+            self.assertEqual(mobile, '0728655088')
+
+
+
+
 
 
 if __name__ == "__main__":
